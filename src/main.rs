5e6fee5438a0eae177c44df94acb25415912d3f1
iuuybsy <18803722821@163.com>
2026-06-21 20:06:40 +0800

    Implementation of basic game structure.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use ggez::{
+    conf::Conf,
+    event::{self, EventHandler},
+    *,
+};
+
+struct SnakeGameState {}
+
+impl SnakeGameState {
+    pub fn new() -> Self {
+        SnakeGameState {}
+    }
+}
+
+impl EventHandler for SnakeGameState {
+    fn draw(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
+        Ok(())
+    }
+
+    fn update(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello world");
+    let mut snake_conf = Conf::new();
+    snake_conf.window_setup.title = "Rust Snake By GGEZ".to_string();
+
+    let (snake_context, snake_game_loop) = ContextBuilder::new("snake", "zdy")
+        .default_conf(snake_conf)
+        .build()
+        .unwrap();
+
+    let snake_game_state = SnakeGameState::new();
+
+    let _ = event::run(snake_context, snake_game_loop, snake_game_state);
 }
